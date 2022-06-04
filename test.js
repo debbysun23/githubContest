eec44b2173036ea2a49f559aadd94a6c2486a5cd
debbysun23 <deborahokafor7@gmail.com>
2022-06-04 21:13:46 +0100

    edited file

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 document.querySelector('.btn').addEventListener('click', function () {
-    window.location.href = `C:/Users/deborah/Documents/front-end/gitContest/index.html`
+    window.location.href = `C:/Users/deborah/Documents/front-end/gitContest/index2.html`
 });
 
